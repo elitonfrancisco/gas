2a7cabb24b7171a1792d193a85869ef52b4bdbae
Fábio Penha <fpenha@gmail.com>
2018-11-29 09:38:21 -0300

    melhorado o script de bando de dados

--- script-criacao.sql
+++ script-criacao.sql
@@ -5,7 +5,7 @@ create table interesados (
 codigo int  auto_increment ,
 nome varchar(255),
 cpf varchar(11),
-primary key (cod_cliente)
+primary key (codigo)
 );
 create table grupos_de_estudos(
 
@@ -15,6 +15,17 @@ assunto varchar(150),
 quando datetime,
 tempo_de_duracao int,
 quantidade_de_pessoas int,
-primary key (codigo)
+interessado_id int,
+primary key (codigo),
+foreign key (interessado_id) references interessados(codigo)
+);
+
+create table interessados_grupos (
+codigo int auto_increment,
+interessado_id int,
+grupo_id int,
+primary key (codigo),
+foreign key (interessado_id) references interessados(codigo),
+foreign key (grupo_id) references grupos_de_estudos(codigo)
 
 );
